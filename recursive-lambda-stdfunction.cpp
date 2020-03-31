68031434c902908cb811a807399f2178043bfa1e
Eliah Kagan <degeneracypressure@gmail.com>
2020-03-31 15:09:21 -0400

    added std::function version but it has a crash bug

---
+++ recursive-lambda-stdfunction.cpp
@@ -0,0 +1,46 @@
+#include <cstddef>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <tuple>
+#include <unordered_map>
+#include <boost/multiprecision/cpp_int.hpp>
+
+int main()
+{
+    using boost::multiprecision::cpp_int;
+
+    constexpr auto hash = [](const std::tuple<cpp_int, unsigned>& key) {
+        constexpr std::size_t seed {127}, multiplier {131'071};
+        const auto& [base, exponent] = key;
+
+        auto prehash = seed;
+        prehash = prehash * multiplier + std::hash<cpp_int>{}(base);
+        prehash = prehash * multiplier + exponent;
+        return prehash;
+    };
+
+    std::unordered_map<std::tuple<cpp_int, unsigned>, cpp_int, decltype(hash)>
+    memo {8, hash};
+
+    std::function<cpp_int(cpp_int, unsigned)>
+    my_pow = [&memo, &my_pow](const cpp_int& base, unsigned exponent) {
+        auto p = memo.find({base, exponent});
+        if (p != end(memo)) return p->second;
+
+        auto result = my_pow(base, exponent / 2);
+        result *= result;
+        if (exponent % 2 != 0) result *= base;
+
+        memo.emplace(std::tuple{base, exponent}, result);
+        return result;
+    };
+
+    auto ans = my_pow(7, 1013);
+    std::cout << "Computed value: " << ans << '\n';
+
+    auto known = pow(cpp_int{7}, 1013);
+    std::cout << "Accepted value: " << known << '\n';
+
+    std::cout << "Correct?  " << std::boolalpha << (ans == known) << '\n';
+}
